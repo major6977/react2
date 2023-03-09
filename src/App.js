c9d1c6e6687c2e341e1dd22b78f246d0b1f23a1d
major6977 <majordav@msn.com>
2023-03-09 11:52:39 -0700

    adding to do list using crud

--- src/App.js
+++ src/App.js
@@ -2,27 +2,41 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
-  const increase = () => {
-    setCount(count + 1);
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
   };
 
-  const decrease = () => {
-    setCount(count - 1);
+  const addTask = () => {
+    const newTodoList = [...todoList, newTask];
+    setTodoList(newTodoList);
   };
 
-  const setToZero = () => {
-    setCount(0);
+  const deleteTask = (taskName) => {
+    const newTodoList = todoList.filter((task) => {
+      return task !== taskName;
+    });
+    setTodoList(newTodoList);
   };
 
   return (
     <div className="App">
-      <button onClick={increase}>Increase</button>
-      <button onClick={decrease}>Decrease</button>
-      <button onClick={setToZero}>Set To Zero</button>
-
-      {count}
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button onClick={addTask}> Add Task </button>
+      </div>
+      <div className="list">
+        {todoList.map((task) => {
+          return (
+            <div>
+              <h1>{task}</h1>
+              <button onClick={() => deleteTask(task)}> X </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
