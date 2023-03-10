223705fa13e8f5999506092a51da2be64b84f0e0
major6977 <majordav@msn.com>
2023-03-10 10:56:00 -0700

    life cycles of components, mounting,unmounting and updating

--- src/App.js
+++ src/App.js
@@ -1,42 +1,21 @@
 import "./App.css";
 import { useState } from "react";
+import { Text } from "./Text";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState("");
-
-  const handleChange = (event) => {
-    setNewTask(event.target.value);
-  };
-
-  const addTask = () => {
-    const newTodoList = [...todoList, newTask];
-    setTodoList(newTodoList);
-  };
-
-  const deleteTask = (taskName) => {
-    const newTodoList = todoList.filter((task) => {
-      return task !== taskName;
-    });
-    setTodoList(newTodoList);
-  };
+  const [showText, setShowText] = useState(false);
 
   return (
     <div className="App">
-      <div className="addTask">
-        <input onChange={handleChange} />
-        <button onClick={addTask}> Add Task </button>
-      </div>
-      <div className="list">
-        {todoList.map((task) => {
-          return (
-            <div>
-              <h1>{task}</h1>
-              <button onClick={() => deleteTask(task)}> X </button>
-            </div>
-          );
-        })}
-      </div>
+      <button
+        onClick={() => {
+          setShowText(!showText);
+        }}
+      >
+        Show Text
+      </button>
+
+      {showText && <Text />}
     </div>
   );
 }
