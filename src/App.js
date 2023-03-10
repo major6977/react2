4eeee79f063ca86243ff7a88dc8cc058a125ee49
major6977 <majordav@msn.com>
2023-03-10 11:53:54 -0700

    fecthinh data via Axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import "./App.css";
-import { useState } from "react";
-import { Text } from "./Text";
+import Axios from "axios";
 
 function App() {
-  const [showText, setShowText] = useState(false);
+  // fetch ("https://catfact.ninja/fact")
+  // .then((res) => res.json())
+  // .then((data) => {
+  // });
+
+  Axios.get("https://catfact.ninja/fact").then((res) => {
+    console.log(res.data);
+  });
 
   return (
     <div className="App">
-      <button
-        onClick={() => {
-          setShowText(!showText);
-        }}
-      >
-        Show Text
-      </button>
-
-      {showText && <Text />}
+      <button> Generate Cat Fact </button>
+      <p> </p>
     </div>
   );
 }
-
 export default App;
