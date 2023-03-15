08cd7742ae7ad610b87a9fbb547dbdda3257619d
major6977 <majordav@msn.com>
2023-03-15 11:21:03 -0700

    using fetch data and ter operators to predict name,age and count

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import Axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [catFact, setCatFact] = useState("");
-
-  const fetchCatFact = () => {
-    Axios.get("https://catfact.ninja/fact").then((res) => {
-      setCatFact(res.data.fact);
+  const [name, setName] = useState("");
+  const [predictedAgeIs, setPredictedAgeIs] = useState(null);
+  const fetchData = () => {
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+      setPredictedAgeIs(res.data);
     });
   };
-
-  useEffect(() => {
-    fetchCatFact();
-  }, []);
-
   return (
     <div className="App">
-      <button onClick={fetchCatFact}> Generate Cat Fact </button>
-      <p> {catFact} </p>
+      <input
+        placeholder="Ex. major..."
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      />
+      <button onClick={fetchData}> Give The Age </button>
+
+      <h1> Name: {predictedAgeIs?.name}</h1>
+      <h1> Predicted Age Is: {predictedAgeIs?.age}</h1>
+      <h1> Count: {predictedAgeIs?.count}</h1>
     </div>
   );
 }
